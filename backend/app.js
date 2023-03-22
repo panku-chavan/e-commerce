04210fe2c9ee1b0602011c13a72b87b919c77ec4
panku-chavan <pankajchavan963@gmail.com>
2023-03-22 08:48:05 +0530

    client added to backend

--- backend/app.js
+++ backend/app.js
@@ -28,10 +28,10 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "./client")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/index.html"));
 });
 
 // Middleware for Errors
